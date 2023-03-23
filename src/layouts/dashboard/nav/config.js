e7ddbc3b4047707086038e8b2f8822ec452c03d5
hieunmtse150772 <nguyenminhtrunghieu10@gmail.com>
2023-03-23 23:32:35 +0700

    fix config.js

--- src/layouts/dashboard/nav/config.js
+++ src/layouts/dashboard/nav/config.js
@@ -12,11 +12,6 @@ const navConfig = [
     icon: icon('ic_analytics'),
   },
   {
-    title: 'user',
-    path: '/dashboard/user',
-    icon: icon('ic_user'),
-  },
-  {
     title: 'category',
     path: '/dashboard/category',
     icon: icon('ic_cart'),
@@ -27,25 +22,10 @@ const navConfig = [
     icon: icon('ic_cart'),
   },
   {
-    title: 'product',
-    path: '/dashboard/products',
-    icon: icon('ic_cart'),
-  },
-  {
     title: 'blog',
     path: '/dashboard/blog',
     icon: icon('ic_blog'),
-  },
-  {
-    title: 'login',
-    path: '/login',
-    icon: icon('ic_lock'),
-  },
-  {
-    title: 'Not found',
-    path: '/404',
-    icon: icon('ic_disabled'),
-  },
+  }
 ];
 
 export default navConfig;
